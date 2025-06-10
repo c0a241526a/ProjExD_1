d764cb526cf2f1adababc20f4baf81ad7c8d2293
c0a241526a <c0a241526a@edu.teu.ac.jp>
2025-06-10 15:57:29 +0900

    残像問題解決

--- pygame_image.py
+++ pygame_image.py
@@ -19,12 +19,14 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0])#スクリーンsurfaceの(横,縦)にbg_img を貼り付ける　blit(画像,位置)でsurfaceに他のsurfaceを貼り付け
-        screen.blit(bg_img2, [-tmr+1600, 0])#スクリーンが連続しているようにみせる 画像を反転させて画像を繋げる
+        x=tmr%3200#+=だと左へ移動するのためbiltの方は-にする    tmr%3200でtmr=3200の余りを出すので0~3199の間になる
+        screen.blit(bg_img, [-x, 0])#スクリーンsurfaceの(横,縦)にbg_img を貼り付ける　blit(画像,位置)でsurfaceに他のsurfaceを貼り付け
+        screen.blit(bg_img2, [-x+1600, 0])#スクリーンが連続しているようにみせる 画像を反転させて画像を繋げる
+        screen.blit(bg_img, [-x+3200, 0])#元の画像をつなげて背景をループさせる 画像の横幅は1600
         screen.blit(koukaton_img, [300, 200])
         pg.display.update()
-        tmr += 1  #+=だと左へ移動するのためbiltの方は-にする  ここを-=でもよし  
-        clock.tick(200)#clock.tick(n)で1秒にn回while文を実行する
+        tmr += 1  #while1回ごとに増加 
+        clock.tick(2000)#clock.tick(n)で1秒にn回while文を実行する
 
 
 if __name__ == "__main__":#このプログラムから実行されたときのみ実行される
